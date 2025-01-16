f76d5782a4dc8263fb57fdc3171db730a0e1253e
DaveyAgni <devarchit.agnihotri@icloud.com>
2025-01-16 09:49:19 -0500

    Update RayOddToEven.java

--- RayOddToEven.java
+++ RayOddToEven.java
@@ -5,8 +5,39 @@
 
 public class RayOddToEven
 {
-	public static int go(int[] ray)
-	{
-		return 0;
-	}
+    public static int go(int[] ray)
+    {
+        int odd = -1;
+        int even = -1;
+
+        for (int i = 0; i < ray.length; i++)
+        {
+            if (ray[i] % 2 == 1)
+            {
+                odd = i;
+                break;
+            }
+        }
+
+        if (odd == -1)
+        {
+            return -1;
+        }
+
+        for (int i = odd + 1; i < ray.length; i++)
+        {
+            if (ray[i] % 2 == 0)
+            {
+                even = i;
+                break;
+            }
+        }
+
+        if (even == -1)
+        {
+            return -1;
+        }
+
+        return even - odd;
+    }
 }
